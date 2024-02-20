6cb5c1611f7d7cfc948b8ab709596e482ab8bdae
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-02-20 14:33:12 -0500

    Second exercise completed

--- PowerSum/Program.cs
+++ PowerSum/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static int count;
+    static void Main(string[] args)
+    {
+        int x= Int32.Parse(Console.ReadLine());
+        int n= Int32.Parse(Console.ReadLine());
+        int[] powers = GetPowers(x, n);
+        bool[] set = new bool[powers.Length];
+        count = 0;
+        GeneratePowerSet(x, powers, set, 0);
+        Console.WriteLine(count);
+    }
+
+    static void GeneratePowerSet(int x, int[] powers, bool[] set, int index)
+    {
+        if (index == set.Length)
+        {
+            count = CheckSum(x, powers, set) ? count + 1 : count;
+        }
+        else
+        {
+            GeneratePowerSet(x, powers, set, index + 1);
+            set[index] = true;
+            GeneratePowerSet(x, powers, set, index + 1);
+            set[index] = false;
+        }
+    }
+
+    static bool CheckSum(int x, int[] powers, bool[] set)
+    {
+        int sum = 0;
+        for (int i = 0; i < powers.Length; i++)
+        {
+            if (set[i])
+            {
+                sum += powers[i];
+            }
+        }
+        return sum == x;
+    }
+
+    static int[] GetPowers(int x, int n)
+    {
+        int[] powers = new int[(int)Math.Pow(x, 1/(double)n)];
+        for (int i = 0; i < powers.Length; i++)
+        {
+            powers[i] = (int)Math.Pow(i + 1, n);
+        }
+        return powers;
+    }
+
+    static void GenerateTestRandom()
+    {
+        Random r = new Random();
+        int x = r.Next(1, 1000);
+        int n = r.Next(2, 10);
+        int[] powers = GetPowers(x, n);
+        bool[] set = new bool[powers.Length];
+        count = 0;
+        GeneratePowerSet(x, powers, set, 0);
+        Console.WriteLine("x = " + x + ", n = " + n + ", count = " + count);
+    }
+}
