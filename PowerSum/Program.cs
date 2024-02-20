d16b49bbcfafb755105f63ccb8760a1e7e7084ca
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-02-20 14:33:37 -0500

    Refactor the code for testing

--- PowerSum/Program.cs
+++ PowerSum/Program.cs
@@ -1,17 +1,26 @@
-﻿class Program
+﻿namespace PowerSum;
+
+public static class Program
 {
     static int count;
     static void Main(string[] args)
     {
         int x= Int32.Parse(Console.ReadLine());
         int n= Int32.Parse(Console.ReadLine());
+        Console.WriteLine(powerSum(x, n));
+    }
+
+    public static int powerSum(int x, int n)
+    {
         int[] powers = GetPowers(x, n);
         bool[] set = new bool[powers.Length];
         count = 0;
         GeneratePowerSet(x, powers, set, 0);
-        Console.WriteLine(count);
+        return count;
     }
 
+    // Generates the power set of the powers array and 
+    // checks if the sum of the set equals x
     static void GeneratePowerSet(int x, int[] powers, bool[] set, int index)
     {
         if (index == set.Length)
@@ -27,6 +36,7 @@
         }
     }
 
+    // Returns true if the sum of the powers in the set equals x
     static bool CheckSum(int x, int[] powers, bool[] set)
     {
         int sum = 0;
@@ -40,6 +50,7 @@
         return sum == x;
     }
 
+    // Returns an array of all powers of x up to x^(1/n)    
     static int[] GetPowers(int x, int n)
     {
         int[] powers = new int[(int)Math.Pow(x, 1/(double)n)];
@@ -48,17 +59,5 @@
             powers[i] = (int)Math.Pow(i + 1, n);
         }
         return powers;
-    }
-
-    static void GenerateTestRandom()
-    {
-        Random r = new Random();
-        int x = r.Next(1, 1000);
-        int n = r.Next(2, 10);
-        int[] powers = GetPowers(x, n);
-        bool[] set = new bool[powers.Length];
-        count = 0;
-        GeneratePowerSet(x, powers, set, 0);
-        Console.WriteLine("x = " + x + ", n = " + n + ", count = " + count);
     }
 }
