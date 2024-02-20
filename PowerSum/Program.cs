aa564d3669a75fb95903003f06d4d1adc0ee5aa7
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-02-20 17:40:12 -0500

    Refactor the code because large cases may delate the ejecution time

--- PowerSum/Program.cs
+++ PowerSum/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace PowerSum;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace PowerSum;
 
 public static class Program
 {
     static int count;
     static void Main(string[] args)
     {
-        int x= Int32.Parse(Console.ReadLine());
-        int n= Int32.Parse(Console.ReadLine());
+        int x = Int32.Parse(Console.ReadLine());
+        int n = Int32.Parse(Console.ReadLine());
         Console.WriteLine(powerSum(x, n));
     }
 
@@ -15,45 +17,38 @@ public static class Program
         int[] powers = GetPowers(x, n);
         bool[] set = new bool[powers.Length];
         count = 0;
-        GeneratePowerSet(x, powers, set, 0);
+        GeneratePowerSet(x, powers, set, 0, 0);
         return count;
     }
 
     // Generates the power set of the powers array and 
     // checks if the sum of the set equals x
-    static void GeneratePowerSet(int x, int[] powers, bool[] set, int index)
+    static void GeneratePowerSet(int x, int[] powers, bool[] set, int index, int sum)
     {
+        if (sum > x)
+        {
+            return;
+        }
         if (index == set.Length)
         {
-            count = CheckSum(x, powers, set) ? count + 1 : count;
+            if (sum == x)
+            {
+                count++;
+            }
         }
         else
         {
-            GeneratePowerSet(x, powers, set, index + 1);
+            GeneratePowerSet(x, powers, set, index + 1, sum);
             set[index] = true;
-            GeneratePowerSet(x, powers, set, index + 1);
+            GeneratePowerSet(x, powers, set, index + 1, sum + powers[index]);
             set[index] = false;
         }
-    }
-
-    // Returns true if the sum of the powers in the set equals x
-    static bool CheckSum(int x, int[] powers, bool[] set)
-    {
-        int sum = 0;
-        for (int i = 0; i < powers.Length; i++)
-        {
-            if (set[i])
-            {
-                sum += powers[i];
-            }
-        }
-        return sum == x;
     }
 
     // Returns an array of all powers of x up to x^(1/n)    
     static int[] GetPowers(int x, int n)
     {
-        int[] powers = new int[(int)Math.Pow(x, 1/(double)n)];
+        int[] powers = new int[(int)Math.Pow(x, 1 / (double)n) + 1];
         for (int i = 0; i < powers.Length; i++)
         {
             powers[i] = (int)Math.Pow(i + 1, n);
