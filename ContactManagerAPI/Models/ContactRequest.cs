b0ac293b7f04d73d9d709b95cdfd29e8d431b586
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-02-21 22:18:28 -0500

    Added the validation to Contact request for posts and put requests

--- ContactManagerAPI/Models/ContactRequest.cs
+++ ContactManagerAPI/Models/ContactRequest.cs
@@ -1,16 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using ContactManagerAPI.Validators;
+
 namespace ContactManagerAPI.Models;
 
 public class ContactRequest
 {
+    [Required]
+    [StringLength(128)]
     public string? Firstname {get; set;}
 
+    [StringLength(128)]
     public string? LastName {get; set;}
 
+    [Required]
+    [StringLength(128)]
+    [EmailValidation]
     public string? Email {get; set;}
 
+    [Required]
+    [DataType(DataType.Date)]
+    [AgeValidator]
     public DateTime? DateOfBirth {get; set;}
 
+    [Required]
+    [StringLength(20)]
     public string? Phone {get; set;}
 
+    [Required]
     public virtual Guid User {get; set;}
 }
